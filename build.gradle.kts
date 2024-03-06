15da520213c678e26e9cd2fe91273753f2f30c6b
maxth <maxthalehim@icloud.com>
2024-03-06 21:07:11 +0100

    fix dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,26 +23,10 @@ val exposed = property("exposed_version")
 dependencies {
     implementation("javax.xml.bind:jaxb-api:2.4.0-b180830.0359")
     implementation("junit:junit:4.12")
-    //implementation("org.glassfish.jaxb:jaxb-runtime:2.3.0")
-    //implementation("de.mxgu:mxpaper:1.2.2")
-    //implementation("org.testng:testng:7.1.0")
     implementation(kotlin("stdlib"))
     implementation(kotlin("reflect"))
-    //implementation("org.jetbrains.exposed:exposed-core:$exposed")
-    //implementation("org.jetbrains.exposed:exposed-jdbc:$exposed")
     compileOnly("io.papermc.paper:paper-api:1.20.4-R0.1-SNAPSHOT")
-
-    // Hibernate dependencies
     implementation("org.hibernate:hibernate-core:5.1.0.Final")
-    //implementation("org.hsqldb:hsqldb:2.3.3")
-
-    //implementation("com.sun.xml.bind:jaxb-core:2.3.0.1")
-    //implementation("com.sun.xml.bind:jaxb-impl:2.3.1")d
-    //implementation("org.javassist:javassist:3.25.0-GA")
-    //implementation("javax.activation:activation:1.1")
-    //implementation("javax.activation:activation:1.1")
-   // implementation("jakarta.xml.bind:jakarta.xml.bind-api:3.0.0")
-    //implementation("org.eclipse.persistence:org.eclipse.persistence.moxy:3.0.0")
 }
 
 tasks {
